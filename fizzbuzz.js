95aadb07e07a536b8e0cecc83c30b4e94bcc4afe
Robert De Leon <robert.deleon@softwire.com>
2024-07-01 15:05:45 +0100

    Implemented FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,7 +6,17 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
 
     // Put your code here...
-
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log(`${i} - FizzBuzz`);
+        } else if (i % 5 == 0) {
+            console.log(`${i} - Buzz`);
+        } else if (i % 3 == 0) {
+            console.log(`${i} - Fizz`);
+        } else {
+            console.log(`${i}`);
+        }
+    }
     
 }
 
