7ccaa4723dbcdd2c92bc7267dd2e7ccffd203b41
Robert De Leon <robert.deleon@softwire.com>
2024-07-03 09:22:54 +0100

    Added .gitignore file
    Update fizzbuzz.js with stretch goals

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,22 +2,33 @@
 
 // Here, we create our main function.
 function fizzbuzz() {
-    
+
     console.log('Welcome to FizzBuzz!');
 
     // Put your code here...
-    for (let i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            console.log(`${i} - FizzBuzz`);
-        } else if (i % 5 == 0) {
-            console.log(`${i} - Buzz`);
-        } else if (i % 3 == 0) {
-            console.log(`${i} - Fizz`);
-        } else {
-            console.log(`${i}`);
+    for (let i = 1; i <= 10000; i++) {
+        let result = "";
+
+        if (i % 3 === 0) result = "Fizz";
+        if (i % 13 === 0) result += "Fezz";
+        if (i % 5 === 0) result += "Buzz";
+        if (i % 7 === 0) result += "Bang";
+        if (i % 11 === 0)
+            if (result === "Fezz")
+                result += "Bong";
+            else result = "Bong";
+        if (i % 17 == 0) {
+            if (result.length === 12) {
+
+            } else {
+                
+            }
+            console.log(`17: ${i} ${result}`);
         }
+
+        // result == "" ? console.log(i) : console.log(result);
     }
-    
+
 }
 
 // Now we run the main function...
